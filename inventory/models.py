d6583d72d60587a5d790d1cc63d1c053e64d41b6
Derrick Ameho <amehoderrick@gmail.com>
2026-06-06 22:08:25 +0100

    Added Category model and Product relationship

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
     name = models.CharField(max_length=100)
     sku = models.CharField(max_length=50, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
