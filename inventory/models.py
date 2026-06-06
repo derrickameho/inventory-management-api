aeff3fbed4ae2cdd173dc1e5f1d7c20b252b2c8a
Derrick Ameho <amehoderrick@gmail.com>
2026-06-06 21:28:04 +0100

    Added inventory app and product model

---
+++ inventory/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
