e4aeb83f802999f33d92cb05cafcba15d3d9b5db
Derrick Ameho <amehoderrick@gmail.com>
2026-06-08 22:46:23 +0100

    Added pagination support

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -10,6 +10,12 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
+
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source='category',
+        write_only=True
+    )
     
     class Meta:
         model = Product
