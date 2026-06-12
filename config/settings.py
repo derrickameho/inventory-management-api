f1cd72af253030e4bf5a2358788ca479e831dbb4
Derrick Ameho <amehoderrick@gmail.com>
2026-06-12 01:00:18 +0100

    Fixed Render CSRF settings

--- config/settings.py
+++ config/settings.py
@@ -31,10 +31,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "inventory-api-tn4x.onrender.com",
+    ".onrender.com",
     "localhost",
     "127.0.0.1",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://inventory-api-tn4x.onrender.com",
+]
 
 # Application definition
 
